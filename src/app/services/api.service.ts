c6b27f8fb38c48f358519e90b3a6632b3c593430
Sam Naser <sammnaser@gmail.com>
2017-08-19 20:47:26 -0400

    Recommendations page added

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -18,6 +18,12 @@ export class ApiService{
       .map((res: Response) => res.json())
   }
 
+  public get_recommended_articles(recommendationType: string): Observable<Article[]>{
+    let recommmendationEndpoint = this.baseEndpoint + 'recommendations/' + recommendationType;
+    return this._http.get(recommmendationEndpoint)
+      .map((res: Response) => res.json())
+  }
+
   //get auth token for username password combination
   //if token given in resonse, store in local storage
   public get_auth_token(username: string, password: string) {
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ApiService } from '../../app/services/api.service';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Article } from '../../app/models/article.model';
+import { Storage } from "@ionic/storage"
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html'
+  templateUrl: 'about.html',
+  providers: [ApiService]
 })
+
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  articles: Article[];
+
+  constructor(
+    public navCtrl: NavController,
+    private _api: ApiService,
+    private _storage: Storage,
+    private _browser: InAppBrowser
+  ) {}
+
+  //when the view enters, load recommended articles
+  //TODO grabbing auth token should be done at login
+  ionViewDidLoad(){
+
+    this._api.get_recommended_articles('user-based-knn/3')
+        .subscribe(data => this.articles = data)
 
   }
 
